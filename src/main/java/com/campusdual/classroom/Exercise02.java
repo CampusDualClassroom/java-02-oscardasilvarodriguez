4b12d73d01a44c456d6810101c43f3cfc45a1ca7
oscardasilvarodriguez <grelodj@hotmail.com>
2024-10-23 14:56:28 +0200

    Ejercicio-o2 refactorizando commits

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -5,9 +5,9 @@ public class Exercise02 {
     public static void main(String[] args) {
         double r = 15;
         double a, l;
-        a = r * r * Math.PI;
-        // Fórmula del área de un círculoS: a = radio * radio * Número PI
 
+        // Fórmula del área de un círculoS: a = radio * radio * Número PI
+        a = r * r * Math.PI;
         System.out.println("Area: " + a);
 
         // Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
