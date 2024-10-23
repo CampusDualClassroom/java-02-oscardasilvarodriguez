cd4439a544d8aab77b6054d1be4dbd66776c3bc4
oscardasilvarodriguez <grelodj@hotmail.com>
2024-10-23 14:41:05 +0200

    Ejercicio-o2 resuelto

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -5,13 +5,13 @@ public class Exercise02 {
     public static void main(String[] args) {
         double r = 15;
         double a, l;
-
-        // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
+        a = r * r * Math.PI;
+        // Fórmula del área de un círculoS: a = radio * radio * Número PI
 
         System.out.println("Area: " + a);
 
-        // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
-
+        // Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
+        l = 2 * r * Math.PI;
         System.out.println("Longitud:  " + l);
 
     }
